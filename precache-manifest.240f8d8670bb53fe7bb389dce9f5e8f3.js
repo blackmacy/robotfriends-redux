dd01f21907df758607d09d546587de2c488278ae
blacmacy <macyleeyijin95@gmail.com>
2019-12-03 13:46:24 -0800

    Updates

---
+++ precache-manifest.240f8d8670bb53fe7bb389dce9f5e8f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "341b67a31855f8d0800a5380b7bfe5c0",
+    "url": "/robotfriends-redux/index.html"
+  },
+  {
+    "revision": "74bd30b564411e6ad29d",
+    "url": "/robotfriends-redux/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "df11814085e1461bddb2",
+    "url": "/robotfriends-redux/static/css/main.cead08b1.chunk.css"
+  },
+  {
+    "revision": "74bd30b564411e6ad29d",
+    "url": "/robotfriends-redux/static/js/2.170d3b0d.chunk.js"
+  },
+  {
+    "revision": "df11814085e1461bddb2",
+    "url": "/robotfriends-redux/static/js/main.3ef11bc4.chunk.js"
+  },
+  {
+    "revision": "5f982f079b112591f991",
+    "url": "/robotfriends-redux/static/js/runtime-main.d5614637.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotfriends-redux/static/media/SEGA.12a3db8b.woff"
+  }
+]);
